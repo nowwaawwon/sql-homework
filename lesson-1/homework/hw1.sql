0407e576e9824785e1ea0b2d09ad6e22f15f950b
nowwaawwon <88283550+nowwaawwon@users.noreply.github.com>
2025-06-23 00:24:09 +0500

    Update hw1.sql

--- lesson-1/homework/hw1.sql
+++ lesson-1/homework/hw1.sql
@@ -1,2 +1,22 @@
-Select 
-Front
+USE [NORTHWND]
+GO
+
+INSERT INTO [dbo].[Employees]
+           ([LastName]
+           ,[FirstName]
+           ,[Title]
+           ,[TitleOfCourtesy],
+		   Region
+           ,[BirthDate]
+           ,[ReportsTo]
+           )
+     VALUES
+           ('Abdullayev',
+		   'Said',
+		   'DBA',
+		   '',
+		   null,
+		   '19810707',
+		   2
+		   )
+GO
